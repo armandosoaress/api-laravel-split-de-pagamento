18523407603621c47331a84e671194191f1b369d
armandosoaress <linkssoares123@gmail.com>
2023-01-06 16:43:15 -0300

    up

--- app/Http/Controllers/Api/Admin/AdminController.php
+++ app/Http/Controllers/Api/Admin/AdminController.php
@@ -528,7 +528,6 @@ class AdminController extends Controller
 
             $idmotoboy = $request->id_motoboy;
 
-            // return $request->id_motoboy;
 
 
             $motoboynumregister = DB::table('dados_hirarquia_pagamento')
